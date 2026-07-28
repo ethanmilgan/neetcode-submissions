947862b9a7deb5aebb07b0871d1dbf7f23d65fd0
Ethan Milgan <79839141+ethanmilgan@users.noreply.github.com>
2026-07-28 12:00:16 -0400

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                if left != right:
+                    nums[left], nums[right] = nums[right], nums[left]
+                left += 1
