d052c1ec1bbf9d249da4fb204cc2819dc5855e05
Ethan Milgan <79839141+ethanmilgan@users.noreply.github.com>
2026-07-31 10:27:58 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights) - 1
+        max_water = 0
+
+        while left < right:
+            width = right - left
+            container_height = min(heights[left], heights[right])
+            max_water = max(max_water, width * container_height)
+
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
