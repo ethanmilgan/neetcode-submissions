2f1c8fea89ba0753671d43e9fa3a1f1433be4352
Ethan Milgan <79839141+ethanmilgan@users.noreply.github.com>
2026-08-06 19:43:25 -0400

    Add: find-common-characters - submission-0

---
+++ Data Structures & Algorithms/find-common-characters/submission-0.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        common = Counter(words[0])
+
+        for word in words[1:]:
+            common &= Counter(word)
+
+        result = []
+
+        for char, count in common.items():
+            result.extend([char] * count)
+
+        return result
