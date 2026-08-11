0fba9dd84ddbe8f6608eab59a386594239cd9679
Ethan Milgan <79839141+ethanmilgan@users.noreply.github.com>
2026-08-11 17:29:59 -0400

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        people = []
+
+        for i in range(len(names)):
+            people.append((heights[i], names[i]))
+
+        people.sort(reverse=True)
+
+        result = []
+
+        for height, name in people:
+            result.append(name)
+
+        return result
